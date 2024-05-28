4d850dd742bcff2e6b0b579ce16d47efbd75a2ec
Esraa Abduallah <esraahisham753@gmail.com>
2024-05-28 17:02:47 +0000

    Add dynamic routes and handle images

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,11 @@
 import { defineCollection, z } from "astro:content";
 
 const postsCollection = defineCollection({
-  schema: z.object({
+  schema: ({image}) => z.object({
     title: z.string(),
     author: z.string(),
     date: z.string(),
-    image: z.string(),
+    image: image(),
   }),
 });
 
